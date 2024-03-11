705aaf4a8ec4893c6697fb43c11a1c9e56e313ab
Khaled AL Asadi <khelo.asadi@gmail.com>
2024-03-11 19:48:30 +0100

    Changes accordingly to handle log in/out

--- main/urls.py
+++ main/urls.py
@@ -17,9 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from myapp import views as index_views
+from django.views.generic.base import TemplateView  # new
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('',index_views.index, name='index')
+     path("", TemplateView.as_view(template_name="home.html"), name="home"),
+    path("accounts/", include("django.contrib.auth.urls")),
     
 ]
