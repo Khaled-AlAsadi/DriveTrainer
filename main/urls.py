fa2545d59d15160be9eed9894e2d00e15f176c77
Khaled AL Asadi <khelo.asadi@gmail.com>
2024-03-12 18:55:52 +0100

    Add ability to singup with email field

--- main/urls.py
+++ main/urls.py
@@ -21,7 +21,8 @@ from django.views.generic.base import TemplateView  # new
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-     path("", TemplateView.as_view(template_name="home.html"), name="home"),
+    path("accounts/", include("accounts.urls")),
+    path("", TemplateView.as_view(template_name="home.html"), name="home"),
     path("accounts/", include("django.contrib.auth.urls")),
     
 ]
