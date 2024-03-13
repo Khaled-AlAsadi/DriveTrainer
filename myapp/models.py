066d0bd60050177191e0191c0ea2b93d3b8f84c8
Khaled AL Asadi <khelo.asadi@gmail.com>
2024-03-13 20:48:30 +0100

    Add models

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Question(models.Model):
+    DIFFICULTY_CHOICES = [
+        ("Easy", "Easy"),
+        ("Medium", "Medium"),
+        ("Hard", "Hard"),
+    ]
+    question_text = models.CharField(max_length=500)
+    pub_date = models.DateTimeField('date published')
+    difficulty = models.CharField(max_length=10, choices=DIFFICULTY_CHOICES)
+
+    def __str__(self):
+        return self.question_text
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    is_correct = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.choice_text
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    selected_choices = models.ManyToManyField(Choice)
+    is_answered = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.question.question_text
