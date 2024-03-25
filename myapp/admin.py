f70615198640db5d0ba2582c76d36b669d218f59
Khaled AL Asadi <khelo.asadi@gmail.com>
2024-03-25 23:10:58 +0100

    Add new models for the new page

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.contrib import admin
-from .models import Question, Choice, Answer
+from .models import Question, Choice, Answer, TraficRule, TraficRuleText
 # Register your models here.
 
 admin.site.register(Question)
 admin.site.register(Choice)
 admin.site.register(Answer)
+admin.site.register(TraficRule)
+admin.site.register(TraficRuleText)
--- myapp/models.py
+++ myapp/models.py
@@ -16,7 +16,6 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
-
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
@@ -34,3 +33,17 @@ class Answer(models.Model):
 
     def __str__(self):
         return self.question.question_text
+
+class TraficRule(models.Model):
+    title = models.CharField(max_length=400)
+    
+    def __str__(self):
+        return self.title
+    
+class TraficRuleText(models.Model):
+    sub_title = models.ForeignKey(TraficRule,on_delete=models.CASCADE)
+    text = models.CharField(max_length=5000)
+    image_link = models.CharField(max_length=2000)
+    
+    def __str__(self):
+        return str(self.sub_title)
