62ee0502db352a9e99b4585faecfece50fb58633
Khaled AL Asadi <khelo.asadi@gmail.com>
2024-03-31 13:46:16 +0200

    Add view for the start quiz

--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,7 @@
 from urllib.request import HTTPRedirectHandler
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
-from .models import Question, Choice,Answer, TraficRule, TraficRuleText
+from django.shortcuts import redirect, render
+from .models import Question, Choice, Answer, TraficRule, TraficRuleText
 from django.contrib.auth.models import AnonymousUser
 from django.shortcuts import get_object_or_404
 
@@ -13,8 +13,9 @@ def index(request):
         print(request.user)
         questions = Question.objects.all()
         total_questions = Question.objects.count()
-        answered_questions = Answer.objects.filter(user=request.user, is_answered=True).count()
-    
+        answered_questions = Answer.objects.filter(
+            user=request.user, is_answered=True).count()
+
         if total_questions > 0:
             progress_percentage = (answered_questions / total_questions) * 100
         else:
@@ -24,26 +25,34 @@ def index(request):
     else:
         return HttpResponseRedirect('login')
 
+
 def trafic_rules(request):
     if not isinstance(request.user, AnonymousUser):
         rules = TraficRule.objects.all()
         rules_texts = TraficRuleText.objects.select_related('sub_title').all()
         print(rules)
-        return render(request, 'trafic_rules.html',{"rules":rules, "rule_texts": rules_texts})
+        return render(request, 'trafic_rules.html', {"rules": rules, "rule_texts": rules_texts})
     else:
         return HttpResponseRedirect('login')
+
 
 def saved_questions(request):
     if not isinstance(request.user, AnonymousUser):
         if request.method == "GET":
             saved_questions = Question.objects.filter(is_saved=True)
-            return render(request, 'saved_questions.html',{'saved_questions': saved_questions})
+            return render(request, 'saved_questions.html', {'saved_questions': saved_questions})
     else:
-        return HttpResponseRedirect('login') 
+        return HttpResponseRedirect('login')
+
+
+def start_quiz(request):
+    first_question = Question.objects.first()
+    return redirect('question_detail', question_id=first_question.id)
+
 
 def continue_quiz(request):
-    questions= Answer.objects.filter(Question,is_answered =False)
-    return render(request, 'question_detail.html', {'questions': questions})
+    first_question = Question.objects.first()
+    return redirect('question_detail', question_id=first_question.id)
 
 def question_detail(request, question_id):
     if not isinstance(request.user, AnonymousUser):
@@ -53,32 +62,36 @@ def question_detail(request, question_id):
         is_answered_correctly = False
 
         if request.method == "POST":
-                submitted_answer_id = request.POST.get('choice')
-                submitted_choice = Choice.objects.get(pk=submitted_answer_id)
-                if submitted_choice.is_correct:
-                    message = "Your answer is correct!"
-                    is_answered_correctly = True
-                    existing_answer = Answer.objects.filter(question=question, user=request.user).first()
-                    if existing_answer:
-                        existing_answer.is_answered = True
-                        existing_answer.save()
-                    else:
-                        answer = Answer.objects.create(question=question, user=request.user, is_answered=True)
-                        answer.selected_choices.add(submitted_choice)
-                    existing_answer = Answer.objects.filter(question=question, user=request.user).first()
-                    for choice in choices:
-                        if choice in existing_answer.selected_choices.all():
-                            choice.is_selected = True
+            submitted_answer_id = request.POST.get('choice')
+            submitted_choice = Choice.objects.get(pk=submitted_answer_id)
+            if submitted_choice.is_correct:
+                message = "Your answer is correct!"
+                is_answered_correctly = True
+                existing_answer = Answer.objects.filter(
+                    question=question, user=request.user).first()
+                if existing_answer:
+                    existing_answer.is_answered = True
+                    existing_answer.save()
                 else:
-                    message = "Sorry, your answer is wrong."
+                    answer = Answer.objects.create(
+                        question=question, user=request.user, is_answered=True)
+                    answer.selected_choices.add(submitted_choice)
+                existing_answer = Answer.objects.filter(
+                    question=question, user=request.user).first()
+                for choice in choices:
+                    if choice in existing_answer.selected_choices.all():
+                        choice.is_selected = True
+            else:
+                message = "Sorry, your answer is wrong."
 
         elif request.method == "GET":
-                existing_answer = Answer.objects.filter(question=question, user=request.user).first()
-                if existing_answer:
-                    for choice in choices:
-                        if choice in existing_answer.selected_choices.all():
-                            choice.is_selected = True
-                    
+            existing_answer = Answer.objects.filter(
+                question=question, user=request.user).first()
+            if existing_answer:
+                for choice in choices:
+                    if choice in existing_answer.selected_choices.all():
+                        choice.is_selected = True
+
         return render(request, 'question_detail.html', {'question': question, 'choices': choices, 'message': message, 'is_answered_correctly': is_answered_correctly})
     else:
-        return HttpResponseRedirect('login') 
+        return HttpResponseRedirect('login')
