b572e7912cf648e879a66e50daab7107fd962c0d
Khaled AL Asadi <khelo.asadi@gmail.com>
2024-04-01 23:17:32 +0200

    Add url for continuing the first quiz

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,14 +1,18 @@
-from django.urls import path,include
+from django.urls import path, include
 from . import views
 
 
 urlpatterns = [
-    path('question/<int:question_id>/', views.question_detail, name='question_detail'),
-    path('saved_questions',views.saved_questions, name='saved_questions'),
-    path("trafic_rules", views.trafic_rules , name="trafic_rules"),
-    path("", views.index , name="home"),
+    path('question/<int:question_id>/',
+         views.question_detail, name='question_detail'),
+    path('saved_questions', views.saved_questions, name='saved_questions'),
+    path("trafic_rules", views.trafic_rules, name="trafic_rules"),
+    path("", views.index, name="home"),
     path('start_quiz', views.start_quiz, name='start_quiz'),
-    path('next_question/<int:current_question_id>/', views.next_question, name='next_question'),
-    path('previous_question/<int:current_question_id>/', views.previous_question, name='previous_question'),
+    path('continue_quiz', views.continue_quiz, name='continue_quiz'),
+    path('next_question/<int:current_question_id>/',
+         views.next_question, name='next_question'),
+    path('previous_question/<int:current_question_id>/',
+         views.previous_question, name='previous_question'),
 
 ]
