7e2b7d40b212192ff0b566ea0b550cec97b5dda9
Khaled AL Asadi <khelo.asadi@gmail.com>
2024-04-10 18:37:30 +0200

    Update to the models

--- myapp/models.py
+++ myapp/models.py
@@ -4,16 +4,16 @@ from django.core.validators import MinLengthValidator, MaxLengthValidator
 
 
 class TraficRuleQuestion(models.Model):
-    question_text = models.CharField(max_length=500,validators=[MinLengthValidator(5), MaxLengthValidator(400)])
+    question_text = models.CharField(max_length=500,validators=[MinLengthValidator(5), MaxLengthValidator(500)])
     pub_date = models.DateTimeField('date published')
     is_saved = models.BooleanField(default=False)
-    link = models.CharField(max_length=500,default="",validators=[MinLengthValidator(5), MaxLengthValidator(400)])
+    link = models.CharField(max_length=500,validators=[MinLengthValidator(5), MaxLengthValidator(500)])
     def __str__(self):
         return self.question_text
 
 class TraficRuleChoice(models.Model):
     question = models.ForeignKey(TraficRuleQuestion, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200,validators=[MinLengthValidator(5), MaxLengthValidator(400)])
+    choice_text = models.CharField(max_length=400,validators=[MinLengthValidator(5), MaxLengthValidator(400)])
     is_correct = models.BooleanField(default=False)
 
     def __str__(self):
@@ -32,15 +32,15 @@ class TraficRuleAnswer(models.Model):
 class TraficRule(models.Model):
     title = models.CharField(max_length=400,validators=[MinLengthValidator(5), MaxLengthValidator(400)])
     sub_title =models.CharField(max_length=400,validators=[MinLengthValidator(5), MaxLengthValidator(400)])
-    sub_text = models.TextField(validators=[MinLengthValidator(5), MaxLengthValidator(1000)],default="Default sub_text")
-    image_link = models.CharField(max_length=400,validators=[MinLengthValidator(5), MaxLengthValidator(500)])
+    sub_text = models.TextField(max_length=1000,validators=[MinLengthValidator(5), MaxLengthValidator(1000)],default="Default sub_text")
+    image_link = models.CharField(max_length=500,validators=[MinLengthValidator(5), MaxLengthValidator(500)])
     def __str__(self):
         return self.title
     
     
 class RoadSign(models.Model):
-    title = models.CharField(max_length=400,validators=[MinLengthValidator(5), MaxLengthValidator(400)])
-    description = models.TextField(validators=[MinLengthValidator(10), MaxLengthValidator(800)],default="Default sub_text")
-    image_link = models.CharField(max_length=400,validators=[MinLengthValidator(10), MaxLengthValidator(500)])
+    title = models.CharField(max_length=400,validators=[MinLengthValidator(5), MaxLengthValidator(400)],blank=False)
+    description = models.TextField(max_length=800,validators=[MinLengthValidator(10), MaxLengthValidator(800)],blank=False)
+    image_link = models.CharField(max_length=500,validators=[MinLengthValidator(10), MaxLengthValidator(500)],blank=False)
     def __str__(self):
         return self.title
