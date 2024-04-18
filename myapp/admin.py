2736f83ae88e633c44c5a4a5cd59a888a5b8c908
Khaled AL Asadi <khelo.asadi@gmail.com>
2024-04-18 22:18:03 +0200

    Add trafic-rule model in admin

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 
-from myapp.models import RoadSign
+from myapp.models import RoadSign, TraficRule
 # Register your models here.
 
 @admin.register(RoadSign)
 class RoadSignAdmin(admin.ModelAdmin):
     list_display = ['title', 'description', 'image_link']
 
+@admin.register(TraficRule)
+class TraficRuleAdmin(admin.ModelAdmin):
+    list_display = ['title', 'sub_title', 'sub_text','image_link']
