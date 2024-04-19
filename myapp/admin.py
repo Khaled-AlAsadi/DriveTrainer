4a9017c4f2631f50eecfe8c1e60082cfa8496cc6
Khaled AL Asadi <khelo.asadi@gmail.com>
2024-04-19 14:05:13 +0200

    Register the rest of the models in admin.py

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from myapp.models import RoadSign, TraficRule
+from myapp.models import RoadSign, TraficRule,TraficRuleAnswer ,TraficRuleChoice, TraficRuleQuestion
 # Register your models here.
 
 @admin.register(RoadSign)
@@ -10,3 +10,18 @@ class RoadSignAdmin(admin.ModelAdmin):
 @admin.register(TraficRule)
 class TraficRuleAdmin(admin.ModelAdmin):
     list_display = ['title', 'sub_title', 'sub_text','image_link']
+
+
+@admin.register(TraficRuleQuestion)
+class TraficRuleQuestion(admin.ModelAdmin):
+    list_display = ['question_text', 'pub_date', 'is_saved','link']
+
+
+@admin.register(TraficRuleChoice)
+class TraficRuleChoice(admin.ModelAdmin):
+    list_display = ['question', 'choice_text', 'is_correct']
+
+
+@admin.register(TraficRuleAnswer)
+class TraficRuleAnswerAdmin(admin.ModelAdmin):
+    list_display = ['question', 'user', 'is_answered']
