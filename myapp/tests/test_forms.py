4f21a0c356e9acd9bc93733c7b83b9e337450acf
Khaled Asadi <khelo.asadi1998@gmail.com>
2024-04-28 21:26:14 +0200

    Add tests for forms

---
+++ myapp/tests/test_forms.py
@@ -0,0 +1,36 @@
+from django.test import SimpleTestCase
+from myapp.forms import RoadSignForm,TraficRuleForm
+
+class TestForms(SimpleTestCase):
+    def test_roadsign_form_valid_data(self):
+        form = RoadSignForm(data={
+            "title":"roadsign 1",
+            "description":"roadsign 1 description",
+            "image_link":"firstimagelink.png"
+        })
+
+        self.assertTrue(form.is_valid())
+    
+    def test_roadsign_form_no_data(self):
+        form = RoadSignForm(data={})
+
+        self.assertFalse(form.is_valid())
+        self.assertEqual(len(form.errors),3)
+    
+
+    def test_traficrule_form_valid_data(self):
+        form = TraficRuleForm(data={
+            "title":"trafic rule 1",
+            "sub_title":"This is the first trafic rule description",
+            "sub_text":"This is the first trafic rule sub_text",
+            "image_link":"firstimagelinkfortraficrule.png"
+        })
+
+        self.assertTrue(form.is_valid())
+
+    def test_traficrule_form_no_data(self):
+        form = RoadSignForm(data={})
+
+        self.assertFalse(form.is_valid())
+        self.assertEqual(len(form.errors),3)
+    
