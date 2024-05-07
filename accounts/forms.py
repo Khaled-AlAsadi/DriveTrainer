03c70528bf3cfffbb096a5ae99e66ced9b45698a
Khaled AL Asadi <khelo.asadi@gmail.com>
2024-05-07 13:30:34 +0200

    Customize the register form style

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,10 +4,13 @@ from django.contrib.auth.models import User
 from django import forms
 # Create your views here.
 class RegisterUserForm(UserCreationForm):
-    email = forms.EmailField()
-    first_name = forms.CharField(max_length=50)
-    last_name = forms.CharField(max_length=50)
-    
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder' :'E-postadress', 'style': 'width: 300px;', 'class': 'form-control'}))
+    first_name = forms.CharField(max_length=50,widget=forms.TextInput(attrs={'placeholder': 'Förnamn', 'style': 'width: 300px;', 'class': 'form-control'}))
+    last_name = forms.CharField(max_length=50,widget=forms.TextInput(attrs={'placeholder': 'Efternamn', 'style': 'width: 300px;', 'class': 'form-control'}))
+    username=forms.CharField(max_length=50,widget=forms.EmailInput(attrs={'placeholder' :'Användarnamn', 'style': 'width: 300px;', 'class': 'form-control'}))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Lösenord', 'style': 'width: 300px;', 'class': 'form-control'}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Lösenord', 'style': 'width: 300px;', 'class': 'form-control'}))
+
     class Meta : 
         model = User
         fields = ('username','first_name','last_name','email','password1','password2')
