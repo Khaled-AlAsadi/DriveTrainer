2ce2deb743f5b91fcaeafc2f4205a6ef6e9e4bfc
Khaled Asadi <khelo.asadi1998@gmail.com>
2024-05-09 21:45:32 +0200

    Format py files #1

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,18 +1,28 @@
 from django import forms
 from .models import RoadSign, TraficRule
- 
- 
+
 # creating a form
+
+
 class RoadSignForm(forms.ModelForm):
-    title = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Title', 'style': 'width: 300px;', 'class': 'form-control'}))
-    description = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Subtitle', 'style': 'width: 300px;', 'class': 'form-control'}))
-    image_link = forms.URLField(widget=forms.URLInput(attrs={'placeholder': 'Image Link', 'style': 'width: 300px;', 'class': 'form-control'}))
+    title = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'Title',
+        'style': 'width: 300px;',
+        'class': 'form-control'}))
+    description = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'Subtitle',
+        'style': 'width: 300px;',
+        'class': 'form-control'}))
+    image_link = forms.URLField(widget=forms.URLInput(attrs={
+        'placeholder': 'Image Link',
+        'style': 'width: 300px;',
+        'class': 'form-control'}))
 
     # create meta class
     class Meta:
         # specify model to be used
         model = RoadSign
- 
+
         # specify fields to be used
         fields = [
             "title",
@@ -20,15 +30,28 @@ class RoadSignForm(forms.ModelForm):
             "image_link",
         ]
 
+
 class TraficRuleForm(forms.ModelForm):
-    title = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Title', 'style': 'width: 300px;', 'class': 'form-control'}))
-    sub_title = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Subtitle', 'style': 'width: 300px;', 'class': 'form-control'}))
-    sub_text = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'Subtext', 'style': 'width: 300px;', 'class': 'form-control'}))
-    image_link = forms.URLField(widget=forms.URLInput(attrs={'placeholder': 'Image Link', 'style': 'width: 300px;', 'class': 'form-control'}))
+    title = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'Title',
+        'style': 'width: 300px;',
+        'class': 'form-control'}))
+    sub_title = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'Subtitle',
+        'style': 'width: 300px;',
+        'class': 'form-control'}))
+    sub_text = forms.CharField(widget=forms.Textarea(attrs={
+        'placeholder': 'Subtext',
+        'style': 'width: 300px;',
+        'class': 'form-control'}))
+    image_link = forms.URLField(widget=forms.URLInput(attrs={
+        'placeholder': 'Image Link',
+        'style': 'width: 300px;',
+        'class': 'form-control'}))
 
     class Meta:
         model = TraficRule
-        
+
         fields = [
             "title",
             "sub_title",
--- myapp/models.py
+++ myapp/models.py
@@ -4,16 +4,23 @@ from django.core.validators import MinLengthValidator, MaxLengthValidator
 
 
 class TraficRuleQuestion(models.Model):
-    question_text = models.CharField(max_length=500,validators=[MinLengthValidator(5), MaxLengthValidator(500)])
+    question_text = models.CharField(max_length=500, validators=[
+                                     MinLengthValidator(5),
+                                     MaxLengthValidator(500)])
     pub_date = models.DateTimeField('date published')
     is_saved = models.BooleanField(default=False)
-    link = models.CharField(max_length=500,validators=[MinLengthValidator(5), MaxLengthValidator(500)])
+    link = models.CharField(max_length=500, validators=[
+                            MinLengthValidator(5), MaxLengthValidator(500)])
+
     def __str__(self):
         return self.question_text
 
+
 class TraficRuleChoice(models.Model):
     question = models.ForeignKey(TraficRuleQuestion, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=400,validators=[MinLengthValidator(5), MaxLengthValidator(400)])
+    choice_text = models.CharField(max_length=400, validators=[
+                                   MinLengthValidator(5),
+                                   MaxLengthValidator(400)])
     is_correct = models.BooleanField(default=False)
 
     def __str__(self):
@@ -29,18 +36,35 @@ class TraficRuleAnswer(models.Model):
     def __str__(self):
         return self.question.question_text
 
+
 class TraficRule(models.Model):
-    title = models.CharField(max_length=400,validators=[MinLengthValidator(5), MaxLengthValidator(400)])
-    sub_title =models.CharField(max_length=400,validators=[MinLengthValidator(5), MaxLengthValidator(400)])
-    sub_text = models.TextField(max_length=500,validators=[MinLengthValidator(5), MaxLengthValidator(500)])
-    image_link = models.CharField(max_length=500,validators=[MinLengthValidator(5), MaxLengthValidator(500)])
+    title = models.CharField(max_length=400, validators=[
+                             MinLengthValidator(5), MaxLengthValidator(400)])
+    sub_title = models.CharField(max_length=400, validators=[
+                                 MinLengthValidator(5),
+                                 MaxLengthValidator(400)])
+    sub_text = models.TextField(max_length=500, validators=[
+                                MinLengthValidator(5),
+                                MaxLengthValidator(500)])
+    image_link = models.CharField(max_length=500, validators=[
+                                  MinLengthValidator(5),
+                                  MaxLengthValidator(500)])
+
     def __str__(self):
         return self.title
-    
-    
+
+
 class RoadSign(models.Model):
-    title = models.CharField(max_length=400,validators=[MinLengthValidator(5), MaxLengthValidator(400)],blank=False)
-    description = models.TextField(max_length=800,validators=[MinLengthValidator(10), MaxLengthValidator(800)],blank=False)
-    image_link = models.CharField(max_length=500,validators=[MinLengthValidator(10), MaxLengthValidator(500)],blank=False)
+    title = models.CharField(max_length=400, validators=[
+                             MinLengthValidator(5),
+                             MaxLengthValidator(400)], blank=False)
+    description = models.TextField(max_length=800, validators=[
+                                   MinLengthValidator(10),
+                                   MaxLengthValidator(800)], blank=False)
+    image_link = models.CharField(max_length=500, validators=[
+                                  MinLengthValidator(10),
+                                  MaxLengthValidator(500)],
+                                  blank=False)
+
     def __str__(self):
         return self.title
