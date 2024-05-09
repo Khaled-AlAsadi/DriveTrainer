83cc7ed8b6c7a5f3687d21ff055e74e0ce0dee57
Khaled Asadi <khelo.asadi1998@gmail.com>
2024-05-09 22:22:48 +0200

    Add more tests for models

--- myapp/tests/test_models.py
+++ myapp/tests/test_models.py
@@ -14,6 +14,23 @@ class TestModels(TestCase):
         w = self.create_roadsign()
         self.assertEqual(w.title, "The first Roadsign title")
 
+    def test_road_sign_update(self):
+        w = self.create_roadsign()
+        updated_title = "Updated roadsign title"
+
+        w.title = updated_title
+        w.save()
+
+        updated_roadsign_title = RoadSign.objects.get(pk=w.pk)
+
+        self.assertEqual(updated_roadsign_title.title, updated_title)
+
+    def test_road_sign_deletion(self):
+        w = self.create_roadsign()
+        initial_count = RoadSign.objects.count()
+        w.delete()
+        self.assertEqual(RoadSign.objects.count(), initial_count - 1)
+
     def create_traficrule(self, title="The first trafic rule title",
                           sub_title="The first trafic rule sub title",
                           sub_text="The first trafic rule sub text",
@@ -26,3 +43,20 @@ class TestModels(TestCase):
     def test_traficrule_creation(self):
         w = self.create_traficrule()
         self.assertEqual(w.title, "The first trafic rule title")
+
+    def test_traffic_rule_deletion(self):
+        w = self.create_traficrule()
+        initial_count = TraficRule.objects.count()
+        w.delete()
+        self.assertEqual(TraficRule.objects.count(), initial_count - 1)
+
+    def test_traffic_rule_update(self):
+        w = self.create_traficrule()
+        updated_title = "Updated traffic rule title"
+
+        w.title = updated_title
+        w.save()
+
+        updated_traffic_rule = TraficRule.objects.get(pk=w.pk)
+
+        self.assertEqual(updated_traffic_rule.title, updated_title)
