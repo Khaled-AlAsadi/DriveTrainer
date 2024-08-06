026de706e77aa0ad80f369ed02f0f7713fca610d
Khaled AL Asadi <khelo.asadi@gmail.com>
2024-08-06 10:05:11 +0200

    Hide sensitive data

--- main/settings.py
+++ main/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-o0*d7=-y9f#9mul01rei$3!o*jz9o9e0ksx731kbk5jle#8&75"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
